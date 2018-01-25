6c673fc21e93ec6393f372a3c829c7ad8c54ff39
Ajeeta Asthana <ajita.asthana@gmail.com>
2018-01-25 07:09:19 +0530

    Modified test2.c

--- test2.c
+++ test2.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 void main()
 {
-	int n,d,x,res=0;
+	int n,d,temp,res=0;
 	printf("Enter a number\n");
 	scanf("%d",&n);
-	x=n;
+	temp=n;
 	while(n!=0)
 	{
 		d=n%10;
 		res=res*10+d;
 		n /= 10;
 	}
-	if(x==n)
+	if(temp==n)
 		printf("Palindrome\n");
 	else
 		printf("Not Palindrome\n");
